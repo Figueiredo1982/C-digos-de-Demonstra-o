7123e61662b31739cdc436dde0894bdab57f8c37
Figueiredo1982 <144946975+Figueiredo1982@users.noreply.github.com>
2025-05-18 13:09:27 -0300

    Update Primos_Linear.py

--- Primos_Linear.py
+++ Primos_Linear.py
@@ -1,23 +1,24 @@
 def prim(n):
   if n < 2:
     return False
-  for i in range (2 int(n** 0,5)+1):
+  for i in range (2, int(n** 0.5)+1):
     if n%i == 0:
       return False
   return True
-  #Acima a validação sobre o número primeiro
+  #Acima a validação sobre o número primo
 
 def listando_os_priminhos_pois_no_exemplo_eles_eram_apresentados_em_formato_de_lista(k):
-  if not instantece(n, int) or n <= 1:
+  if not instantece(k, int) or k <= 1:
     print ("K deveria ser um número maior que 1, além de ser um inteiro")
+    return []
   primos = []
-  n = 2
-  while len(primos) < n:
-    if prim(n):
+  num = 2
+  while len(primos) < k:
+    if prim(num):
       primos.append(n)
-    k += 1
+    num += 1
   return primos
 
-n = input(int("Digite um valor N inteiro maior que 1 : "))
+k = input(int("Digite um valor N inteiro maior que 1 : "))
 print(f"Os primeiros {k} números primos são: {listando_os_priminhos_pois_no_exemplo_eles_eram_apresentados_em_formato_de_lista(k)}")
     
